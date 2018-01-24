4dd283438302b0ff81fbd96fc88e1c0f844b46cb
Jonathan Goodwin <jondgoodwin@gmail.com>
2018-01-25 08:53:00 +1000

    Implement ASM and LLVM IR buttons

--- src/playcone.rb
+++ src/playcone.rb
@@ -48,4 +48,49 @@ class PlayCone < Sinatra::Base
 	content_type :json
 	response
   end
+  
+  post "/compile.json" do
+	# extract and save the code from JSON payload
+	request.body.rewind
+	body = JSON.parse request.body.read
+	File.open("play/test.cone", 'w') { |file| file.write(body['code']) }
+	resultfile = body['emit'] == 'asm' ? "play/test.s" : "play/test.ir"
+	
+	# The list of scripted commands to execute
+    cmds = [
+	  "cone/cone --asm --llvmir -oplay/ play/test.cone",
+	]
+	rc, stdout, error = docmds(cmds)
+
+	if rc == 0
+		output = {'result' => File.read(resultfile)}
+	else
+		output = {'error' => error}
+	end
+	response = JSON.generate(output)
+	content_type :json
+	response
+  end
+end
+
+def docmds(cmds)
+	rc = 0
+	prog_out = ""
+	conec_err = ""
+	
+	# Perform them, one after another, capturing the results. Stop on first fail
+	ENV['PATH'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+	cmds.each do |cmd|
+	  if rc == 0
+		stdout, stderr, status = Open3.capture3(cmd)
+		rc = status.exitstatus
+		if rc == 0
+		  prog_out = stdout
+		else
+		  conec_err = stderr
+		end
+	  end
+	end
+	
+	return rc, prog_out, conec_err
 end
