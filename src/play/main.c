00cec8c7adbd23b7d07dd286f62d1a96186238e0
Jonathan Goodwin <jondgoodwin@gmail.com>
2018-01-25 14:05:19 +1000

    Use cone standard library during linkedit

--- src/play/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stdio.h>
-
-uint32_t cone();
-
-int main() {
-	printf("%d\n", cone());
-}
--- src/playcone.rb
+++ src/playcone.rb
@@ -13,33 +13,21 @@ class PlayCone < Sinatra::Base
 	body = JSON.parse request.body.read
 	File.open("play/test.cone", 'w') { |file| file.write(body['code']) }
 	
-	# The list of scripted commands to execute
-    cmds = [
-	  "cone/cone -oplay/ play/test.cone",
-	  "gcc -c play/main.c -o play/main.o",
-	  "gcc -o play/main play/main.o play/test.o",
-	  "timeout 15s play/main"]
-	
-	rc = 0
+	# Run compile and generate hash object with requested file or error message
 	prog_out = ""
-	conec_err = ""
-	
-	# Perform them, one after another, capturing the results. Stop on first fail
-	ENV['PATH'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-	cmds.each do |cmd|
-	  if rc == 0
-		stdout, stderr, status = Open3.capture3(cmd)
-		rc = status.exitstatus
-		if rc == 0
-		  prog_out = stdout
-		else
-		  conec_err = stderr
-		end
-	  end
-	end
-	
-
+	rc, stdout, conec_err = docmd("cone/cone -oplay/ play/test.cone")
 	if rc == 0
+		# linkedit with the cone standard library. Path is needed for success
+		ENV['PATH'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+		rc, stdout, stderr = docmd("gcc -o play/main play/test.o cone/bin/libconestd.a")
+		if rc == 0
+			rc, prog_out, stderr = docmd("timeout 15s play/main")
+		else
+			conec_err = stderr
+		end
+	end
+
+	if prog_out.size > 0
 		output = {'conec' => conec_err, 'program' => prog_out}
 	else
 		output = {'conec' => conec_err}
@@ -54,43 +42,27 @@ class PlayCone < Sinatra::Base
 	request.body.rewind
 	body = JSON.parse request.body.read
 	File.open("play/test.cone", 'w') { |file| file.write(body['code']) }
-	resultfile = body['emit'] == 'asm' ? "play/test.s" : "play/test.ir"
 	
-	# The list of scripted commands to execute
-    cmds = [
-	  "cone/cone --asm --llvmir -oplay/ play/test.cone",
-	]
-	rc, stdout, error = docmds(cmds)
-
+	# Run compile and generate hash object with requested file or error message
+	rc, stdout, error = docmd("cone/cone --asm --llvmir -oplay/ play/test.cone")
 	if rc == 0
+		# retrieve results on success
+		resultfile = body['emit'] == 'asm' ? "play/test.s" : "play/test.ir"
 		output = {'result' => File.read(resultfile)}
 	else
 		output = {'error' => error}
 	end
+	
+	# Return JSON-encoded requested file or error message 
 	response = JSON.generate(output)
 	content_type :json
 	response
   end
 end
 
-def docmds(cmds)
-	rc = 0
-	prog_out = ""
-	conec_err = ""
-	
-	# Perform them, one after another, capturing the results. Stop on first fail
-	ENV['PATH'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-	cmds.each do |cmd|
-	  if rc == 0
-		stdout, stderr, status = Open3.capture3(cmd)
-		rc = status.exitstatus
-		if rc == 0
-		  prog_out = stdout
-		else
-		  conec_err = stderr
-		end
-	  end
-	end
-	
-	return rc, prog_out, conec_err
+# Run a command and capture output
+def docmd(cmd)
+	stdout, stderr, status = Open3.capture3(cmd)
+	rc = status.exitstatus
+	return rc, stdout, stderr
 end
