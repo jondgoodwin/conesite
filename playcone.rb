bd38c0193b1746c5d23c714c9fac15d5d3a5a987
Jonathan Goodwin <jondgoodwin@gmail.com>
2022-05-08 16:40:02 +1000

    Link without PIE for now

--- playcone.rb
+++ playcone.rb
@@ -19,7 +19,7 @@ class PlayCone < Sinatra::Base
 	if rc == 0
 		# linkedit with the cone standard library. Path is needed for success
 		ENV['PATH'] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-		rc, stdout, stderr = docmd("gcc -o work/test work/test.o /newvolume/cone/conec/libconestd.a")
+		rc, stdout, stderr = docmd("gcc -no-pie -o work/test work/test.o /cone/conec/libconestd.a")
 		if rc == 0
 			norc, prog_out, stderr = docmd("timeout 15s work/test")
 		else
